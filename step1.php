e73a69c4019e791ac69a89001206136a5afc8374
Soufiane <yousfisoufiane8@gmail.com>
2021-07-28 00:21:15 +0100

    Check another time

--- step1.php
+++
@@ -1,6 +0,0 @@
-<?php
-  header('Content-Type: application/javascript; charset=UTF-8');
-  // Set test cookie
-  setcookie('third_party_c_t', 'hey there!', time() + 3600*24*2);
-?>
-window._3rd_party_test_step1_loaded();
